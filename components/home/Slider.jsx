55523128cf3c92f0f96d3da6bbdc81e3ac33f673
ChristopheC83 <christophe.chiappetta@gmail.com>
2024-03-19 07:14:34 +0100

    slider ok

--- components/home/Slider.jsx
+++ components/home/Slider.jsx
@@ -3,22 +3,70 @@
 import Image from "next/image";
 import { useState } from "react";
 import { sliders } from "../../lib/slider";
+import { FaArrowCircleLeft } from "react-icons/fa";
+import { FaArrowCircleRight } from "react-icons/fa";
 
 export default function Slider() {
-  const [sliderNumber, setSliderNumber] = useState(0);
+  const [sliderNumber, setSliderNumber] = useState(1);
 
   console.log(sliders[sliderNumber].image);
 
+  function nextSlider() {
+    if (sliderNumber < sliders.length - 1) {
+      setSliderNumber(sliderNumber + 1);
+    } else {
+      setSliderNumber(0);
+    }
+  }
+
+  function prevSlider() {
+    if (sliderNumber > 0) {
+      setSliderNumber(sliderNumber - 1);
+    } else {
+      setSliderNumber(sliders.length - 1);
+    }
+  }
+
   return (
-    <div className="w-full min-h-[90vh] bg-dark-ve">
+    <div className="w-full min-h-[90vh] bg-dark-ve ">
       <div
-        className={`relative w-full bg-no-repeat bg-${
+        className={`relative w-full bg-no-repeat 
+        bg-${
           sliders[sliderNumber].style ? sliders[sliderNumber].style : "cover"
-        } bg-top lg:bg-center h-[90vh]`}
+        }
+         bg${
+           sliders[sliderNumber].alignX ? sliders[sliderNumber].alignX : ""
+         }-${
+          sliders[sliderNumber].alignY ? sliders[sliderNumber].alignY : "top"
+        }  
+         
+         h-[90vh]`}
         key={sliders[sliderNumber].id}
         style={{ backgroundImage: `url(${sliders[sliderNumber].image})` }}
       >
-        {sliders[sliderNumber].title}
+        <div className="flex flex-col justify-center w-full h-full border border-red-500">
+          <div className="relative w-full h-full text-5xl text-white">
+            <div className="absolute p-4 translate-x-1/2 top-[25%] right-1/2">
+              {sliders[sliderNumber].title}
+            </div>
+            <a
+              href={sliders[sliderNumber].btnLink}
+              className="absolute p-4 translate-x-1/2 border-4 border-white top-[66%] w-[300px] flexMid right-1/2 bg-neutral-900"
+            >
+              {sliders[sliderNumber].btnText}
+            </a>
+            <div className="absolute flex justify-between translate-x-1/2 w-[300px] bottom-16 right-1/2 ">
+              <FaArrowCircleLeft
+                onClick={prevSlider}
+                className="cursor-pointer"
+              />
+              <FaArrowCircleRight
+                onClick={nextSlider}
+                className="cursor-pointer"
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
