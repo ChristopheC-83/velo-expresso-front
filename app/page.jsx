72600b688a351e84cfa4c7838a640fcc4e281615
ChristopheC83 <christophe.chiappetta@gmail.com>
2024-03-19 14:41:43 +0100

    frames overlay ok

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,12 @@
 import Container from "@/components/container/Container";
+import Frames from "@/components/home/Frames";
 import Slider from "@/components/home/Slider";
 
 export default function Home() {
   return (
     <div className="w-full">
       <Slider />
+      <Frames/>
     </div>
   );
 }
--- components/home/Frames.jsx
+++ components/home/Frames.jsx
@@ -3,23 +3,23 @@ import Container from "../container/Container";
 import { frames } from "@/lib/frames";
 
 export default function Frames() {
+  
   return (
     <Container>
       <div className="min-h-screen sm:mt-8 md:mt-12">
-        <div className="grid grid-cols-2 sm:gap-4 md:gap-8">
+        <div className="flex flex-col ">
           {frames.map((frame) => (
             <div key={frame.id} className="relative">
               <Image
                 src={frame.image}
                 alt={frame.title}
                 width={500}
-                height={800}
-                className="object-cover w-full h-full"
+                height={450}
+                className="object-cover w-full h-[450px]"
               />
+              {/* overlay si image trop clair */}
               <div
-                className={`absolute inset-0 bg-black bg-opacity-50 ${
-                  frame.overlay ? "bg-opacity-50" : "bg-opacity-0"
-                }`}
+                className={`absolute inset-0 bg-neutral-900 bg-opacity-${frame.overlay}`}
               ></div>
               <div className="absolute inset-0 flex flex-col items-center justify-center text-white">
                 <h2 className="text-3xl font-bold sm:text-4xl md:text-5xl">
