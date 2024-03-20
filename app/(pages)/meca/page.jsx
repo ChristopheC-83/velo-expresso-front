1298a89e9ac99b9383cb78b5c0f47d1157c06478
ChristopheC83 <christophe.chiappetta@gmail.com>
2024-03-20 17:52:43 +0100

    atelier ok

--- app/(pages)/meca/page.jsx
+++ app/(pages)/meca/page.jsx
@@ -1,17 +1,67 @@
+/* eslint-disable react/no-unescaped-entities */
+// page des tarifs atelier
+"use client";
 
+import HeaderImage from "@/components/HeaderImage";
+import Container from "@/components/container/Container";
+import ArrowToTop from "@/components/home/components/ArrowToTop";
+import { categoriesMeca, meca } from "@/lib/meca";
 
+export default function Atelier() {
+  return (
+    <div className="relative w-full">
+      <ArrowToTop />
+      <HeaderImage image="/images/meca/meca.jpg" title="L'atelier" />
+      <Container>
+        <div className="w-full">
+          <h2 className="m-4 text-xl text-center font-weight sm:text-3xl ">
+            Retrouvez ici les tarifs et les forfaits pour l'entretien de votre
+            vélo.
+          </h2>
 
- export default function Atelier() {
+          <div className="flex flex-wrap justify-center w-11/12 gap-4 pt-4 mx-auto my-6 sm:gap-x-6 md:gap-x-8 md:w-full">
+            {categoriesMeca.map((categorie) => (
+              <div
+                key={categorie.id}
+                className="w-11/12 h-fit text-center border-x-2 border-t-2 border-black md:w-[350px]"
+              >
+                <div className="flex flex-col">
+                  <h3 className="py-4 uppercase border-b-2 border-black bg-ve-blue/50">
+                    {categorie.categorie}
+                  </h3>
 
-
-
-return (
-    <div>
-<h1 className="text-blue-ve">Atelier</h1>
-
-
+                  {meca
+                    .filter((item) => item.categorie === categorie.categorie)
+                    .map((operation) => (
+                      <div
+                        key={operation.id}
+                        className="flex items-center justify-between w-full p-2 border-b-2 border-black"
+                      >
+                        <p>{operation.opération}</p>
+                        <p>{operation.prix} €</p>
+                      </div>
+                    ))}
+                </div>
+              </div>
+            ))}
+            <div className="w-[350px] h-1"></div>
+            <div className="w-[350px] h-1"></div>
+          </div>
+        </div>
+        <div className="w-full mx-auto mb-6 sm:w-4/5 md:w-3/5">
+          <h5 className="mb-2">
+            * Ces tarifs sont estimatifs. Un devis final vous sera communiquer
+            lorsque nous verrons le vélo en magasin.
+          </h5>
+          <h5 className="mb-2">
+            * Une intervention peut mettre à jour une/des défectuosité(s). Dans ce
+            cas, un nouveau devis vous sera adressé.
+          </h5>
+          <h5 className="mb-2">
+            * Les cables internes pouvant prendre du temps à être changés, un supplément s'élevant jusqu'à 90€ pourra être ajouté à la facture.
+          </h5>
+        </div>
+      </Container>
     </div>
-
   );
-
 }
---
+++ components/HeaderImage.jsx
@@ -0,0 +1,18 @@
+export default function HeaderImage({ image, title }) {
+  return (
+    <div className="w-full  h-[100vw] lg:h-[90vh] bg-ve-dark ">
+      <div
+        className={`relative w-full h-[100vw] lg:h-[90vh] bg-no-repeat bg-top aspect-square lg:aspect-[16/9] overflow-hidden bg-cover `}
+        style={{ backgroundImage: `url(${image})` }}
+      >
+        <div className="relative w-full h-full text-white flexMid">
+          <div className={`absolute inset-0 ${"bg-neutral-900/50"}`}></div>
+          <div className="absolute top-[45%]  w-full">
+            <h2 className="text-3xl text-center sm:text-4xl md:text-5xl lg:text-7xl">{title}</h2>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
+}
