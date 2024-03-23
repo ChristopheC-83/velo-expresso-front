cc1b28a30fd625543b732e67a953046e63326248
ChristopheC83 <christophe.chiappetta@gmail.com>
2024-03-23 17:54:34 +0100

    MAJ lien api pour main

--- hooks/useWorkshop.js
+++ hooks/useWorkshop.js
@@ -3,7 +3,10 @@ import axios from "axios";
 
 export function useWorkshop() {
   return useQuery("worshop", async () => {
-    const { data } = await axios.get(`http://localhost:8090/kiki/velo-expresso-back/api_workshop`);
+    // en dev
+    // const { data } = await axios.get(`http://localhost:8090/kiki/velo-expresso-back/api_workshop`);
+    // en ligne
+    const { data } = await axios.get(`https://dbve.barpat.fun/api_workshop`);
     return data;
   });
 }
