8b4c55a25e7954621d07ca9fe460d1cec6410281
ChristopheC83 <christophe.chiappetta@gmail.com>
2024-04-03 19:44:31 +0200

    secret 3

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,9 +1,9 @@
 import { authOptions } from '@/lib/auth-options'
 import NextAuth from 'next-auth/next'
 
+const handler = NextAuth({
+  ...authOptions,
+  secret: process.env.NEXTAUTH_SECRET 
+})
 
-
-const handler = NextAuth(authOptions)
-secret: process.env.NEXTAUTH_SECRET
-
-export {handler as GET, handler as POST}
+export { handler as GET, handler as POST }
