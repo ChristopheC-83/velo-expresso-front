cd81b418e3b3e940da50b2bfd629f72d443aff3d
ChristopheC83 <christophe.chiappetta@gmail.com>
2024-04-07 12:12:59 +0200

    loader personnalisé et errorPage

--- app/loading.jsx
+++ app/loading.jsx
@@ -1,9 +1,20 @@
 import { TfiReload } from "react-icons/tfi";
 
+import Image from "next/image";
 export default function Loader() {
   return (
-    <div className="w-full p-16 min-h-52 bg-neutral-800">
-      <TfiReload className="mx-auto font-bold text-blue-500 text-7xl animate-spin scale-[-1]" />
+    <div className="w-full min-h-screen p-16 bg-neutral-800">
+      {/* <TfiReload className="mx-auto font-bold text-blue-500 text-7xl animate-spin scale-[-1]" /> */}
+      <div className="w-[300px] h-[300px] mx-auto font-bold text-blue-500 text-7xl  flexMid r	">
+      <Image
+        src="/images/loader/loader.png"
+        width={300}
+        height={300}
+        alt="loader"
+        className="animate-loader scale-[-1]"
+      
+      />
+    </div>
     </div>
   );
 }
--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,7 +1,21 @@
+/* eslint-disable react/no-unescaped-entities */
+import Link from "next/link";
+import Image from "next/image";
 export default function ErrorPage() {
   return (
-    <div className="w-full min-h-screen text-3xl font-bold text-white bg-neutral-900 flexMid">
-      ErrorPage
-    </div>
+      <div className="flex-col w-full gap-6 pt-8 pb-16 text-xl font-bold text-white sm:text-2xl md:text-3xl bg-neutral-800 flexMid">
+        <h2>On est peut -être bien là...</h2>
+        <h2>Mais un peu perdu non ?</h2>
+        <Image
+          src="/images/errorPage/errorPage.webp"
+          alt="velo dans les champs"
+          width={400}
+          height={200}
+          className=""
+        />
+        <Link href="/">
+          <h2 className="underline underline-offset-8 ">Retournons à l'accueil ! </h2>
+        </Link>
+      </div>
   );
 }
