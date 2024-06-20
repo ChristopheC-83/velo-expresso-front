ce7dde0d752395c8b17c3a5c10d917b32541099f
ChristopheC83 <christophe.chiappetta@gmail.com>
2024-06-20 13:52:10 +0200

    sitemap

---
+++ sitemap.js
@@ -0,0 +1,36 @@
+
+ 
+export default function sitemap() {
+  return [
+    {
+      url: 'https://velo-expresso.com/',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: 'https://www.velo-expresso.com/new-bikes',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: 'https://www.velo-expresso.com/used-bikes',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: 'https://www.velo-expresso.com/meca',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: 'https://www.velo-expresso.com/rental',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+  ]
+}
